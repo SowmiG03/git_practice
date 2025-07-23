aac4e0eac402f66129fe73127d1616a75c4a3565
Akash A <akashazhagesan12@gmail.com>
2025-07-23 19:02:01 +0530

    new file

---
+++ sample.js
@@ -0,0 +1,8 @@
+var user_name = 'keerthi';
+console.log(user_name);
+var age = 22;
+console.log(age);
+age = 32;
+console.log(age);
+var isadult = age > 15;
+console.log(isadult);
---
+++ sample.ts
@@ -0,0 +1,15 @@
+let user_name:string='keerthi';
+console.log(user_name);
+let age:number=22;
+console.log(age);
+age=32;
+console.log(age);
+let isadult:boolean=age>15;
+console.log(isadult);
+
+
+let information={
+    name:"priya",
+    age:21,
+    blood_grp:
+}
